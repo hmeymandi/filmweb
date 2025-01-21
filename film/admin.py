ddde74087e962fef2ccf500a39f0e37b6ed9bfb2
H.meymandi <hossainm67@gmail.com>
2025-01-21 19:38:53 +0330

    create new model

--- film/admin.py
+++ film/admin.py
@@ -12,9 +12,9 @@ class GenreAdmin(admin.ModelAdmin):
 admin.site.register(Genre, GenreAdmin)
 
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'release_date', 'director', 'genre', 'rating')
-    list_filter = ('release_date', 'director', 'genre')
-    search_fields = ('title', 'director', 'genre')
+    list_display = ('title', 'release_date', 'director', 'rating')
+    list_filter = ('release_date', 'director', )
+    search_fields = ('title', 'director',)
     date_hierarchy = 'release_date'
     ordering = ('title',)
     readonly_fields = ('date_created', 'date_modified')
@@ -27,10 +27,35 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('film', 'author', 'body', 'created_at')
     list_filter = ('film', 'author', 'created_at')
     search_fields = ('film', 'author', 'body')
-    
-    
-    
-    
-
+         
 
 admin.site.register(Comment, CommentAdmin)
+
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'date_of_birth', 'gender')
+    list_filter = ('date_of_birth', 'gender')
+    search_fields = ('full_name', 'date_of_birth')
+    ordering = ('full_name',)
+    prepopulated_fields = {'slug': ('full_name',)}
+
+
+admin.site.register(Actors, ActorAdmin)
+
+class LivetvAdmin(admin.ModelAdmin):
+    list_display = ('title', 'name', 'start_date',)
+    list_filter = ('name', 'start_date',)
+    search_fields = ('title', 'name')
+    date_hierarchy = 'start_date'
+    ordering = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+
+class SeriesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'release_date', 'director', 'rating')
+    list_filter = ('release_date', 'director',)
+    search_fields = ('title', 'director',)
+    date_hierarchy = 'release_date'
+    ordering = ('title',)
+    readonly_fields = ('date_created', 'date_modified')
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Series, SeriesAdmin)
--- film/models.py
+++ film/models.py
@@ -4,6 +4,32 @@ from django.urls import reverse
 
 # Create your models here.
 
+class Livetv(models.Model):
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True)
+    link = models.URLField()
+    image = models.ImageField(upload_to='livetv_images')
+    star_date = models.DateField()
+
+class Actors(models.Model):
+    gender_ch =(('Male', 'Male'), ('Female', 'Female'),)
+
+    full_name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200,unique=True)
+    image = models.ImageField(upload_to='actors_images')
+    gender = models.CharField(max_length=6, choices=gender_ch)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    date_of_birth = models.DateField()
+    biography = models.TextField()
+    date_of_death = models.DateField(null=True, blank=True)
+    date_of_birth_place = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.full_name
+    
+
 class Genre(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, unique=True)
@@ -18,26 +44,47 @@ class Film(models.Model):
     release_date = models.DateField()
     director = models.CharField(max_length=100)
     duration = models.PositiveIntegerField()
-    genre = models.ForeignKey(Genre, on_delete=models.CASCADE,related_name='films')
+    genre = models.ManyToManyField(Genre,related_name='films')
     description = models.TextField()
+    actors = models.ManyToManyField(Actors,related_name='films')
     rating = models.FloatField()
     image = models.ImageField(upload_to='film_images')
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
+    download_link_480 = models.URLField(blank=True)
+    download_link_720 = models.URLField(blank=True)
+    download_link_1080 = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Series(models.Model):
+    title = models.CharField(max_length=200)
+    slug=models.SlugField(max_length=200, unique=True)
+    release_date = models.DateField()
+    director = models.CharField(max_length=100)
+    duration = models.PositiveIntegerField()
+    genre = models.ManyToManyField(Genre,related_name='series')
+    description = models.TextField()
+    actors = models.ManyToManyField(Actors,related_name='series')
+    rating = models.FloatField()
+    image = models.ImageField(upload_to='serirs_images')
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    download_link_480 = models.URLField(blank=True)
+    download_link_720 = models.URLField(blank=True)
+    download_link_1080 = models.URLField(blank=True)
 
     def __str__(self):
         return self.title
     
 class Comment(models.Model):
-    Film_Starts =[('1','very_bad'),
-                  ('2','bad'),
-                  ('3','normal'),
-                  ('4','good'),
-                  ('5','very_good')]
-                  
-                  
-                  
-                  
+    Film_Starts =[('1','خیلی بد'),
+                  ('2','بد'),
+                  ('3','عادی'),
+                  ('4','خوب'),
+                  ('5','عالی')]                 
+                                       
                   
     film = models.ForeignKey(Film, on_delete=models.CASCADE, related_name='comments')           
     author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE,related_name='comments')
